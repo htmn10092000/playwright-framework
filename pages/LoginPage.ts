cd3e03744e074cf32b57fb6c8547a739af049c93
niho-ja <nho@jobadder.com>
2026-01-04 20:28:06 +0700

    read data

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -3,12 +3,12 @@ import { Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class LoginPage extends BasePage {
-  readonly userNameTxt: Locator;
-  readonly passWordTxt: Locator;
-  readonly loginBtn: Locator;
+  private readonly userNameTxt: Locator;
+  private readonly passWordTxt: Locator;
+  private readonly loginBtn: Locator;
 
   constructor(page: Page) {
-    super(page); // ✅ gọi BasePage constructor
+    super(page);
 
     this.userNameTxt = this.page.locator('#userName');
     this.passWordTxt = this.page.locator('#password');
